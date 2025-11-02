d9f532d11cfb4854883d394084397089daa1e70a
Jail Idea <61623614+jailideaid@users.noreply.github.com>
2025-11-02 16:40:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from server import start_api
-import telegram_bot
-import threading
 
 if __name__ == "__main__":
-    threading.Thread(target=start_api).start()
-    telegram_bot.run_bot()
+    start_api()
